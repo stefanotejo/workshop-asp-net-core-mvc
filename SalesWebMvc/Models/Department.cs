a02e82acd6cb9bedd23e7172364f652af8c4e6ca
Stefano Tejo <stefano.tejo@gmail.com>
2020-01-17 18:15:01 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,11 +9,5 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
     }
 }
