4b46451c48f3b8578b2d80009a0c8f48b9cf4ea3
Stefano Tejo <stefano.tejo@gmail.com>
2020-01-21 00:24:49 -0300

    Safety commit. GroupingSearch query is broken (cannot be translated properly). Waiting for an answer from Prof. Nelio

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -39,9 +39,21 @@ namespace SalesWebMvc.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1); // If minDate wasn't given, consider it to be the first day of current year
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now; // If maxDate wasn't given, consider it to be the current date
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd"); // Dictionary ViewData value for key "minDate"
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd"); // Dictionary ViewData value for key "maxDate"
+
+            List<IGrouping<Department, SalesRecord>> result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -43,19 +43,41 @@ namespace SalesWebMvc.Services
         {
             IQueryable<SalesRecord> result = from obj in _context.SalesRecord select obj;
 
-            if (minDate.HasValue)
+            if (minDate.HasValue) // If minDate was given
             {
-                result = result.Where(x => x.CreatedAt >= minDate.Value);
+                result = result.Where(x => x.CreatedAt >= minDate.Value); // Apply minDate restriction
             }
-            if (maxDate.HasValue)
+            if (maxDate.HasValue) // If maxDate was given
             {
-                result = result.Where(x => x.CreatedAt <= maxDate.Value);
+                result = result.Where(x => x.CreatedAt <= maxDate.Value); // Apply maxDate restriction
             }
 
             return await result
                 .Include(x => x.Seller) // Join with table Seller
                 .Include(x => x.Seller.Department) // Join with table Department
                 .OrderByDescending(x => x.CreatedAt) // Ordering by date (decrescent)
+                .ToListAsync(); // db access
+        }
+
+        // Async FindByDateGrouping
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            IQueryable<SalesRecord> result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue) // If minDate was given
+            {
+                result = result.Where(x => x.CreatedAt >= minDate.Value); // Apply minDate restriction
+            }
+            if (maxDate.HasValue) // If maxDate was given
+            {
+                result = result.Where(x => x.CreatedAt <= maxDate.Value); // Apply maxDate restriction
+            }
+
+            return await result
+                .Include(x => x.Seller) // Join with table Seller
+                .Include(x => x.Seller.Department) // Join with table Department
+                .OrderByDescending(x => x.CreatedAt) // Ordering by date (decrescent)
+                .GroupBy(x => x.Seller.Department) // Group by department. This return IGrouping
                 .ToListAsync(); // db access
         }
     }
